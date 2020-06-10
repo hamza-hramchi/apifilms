e80327daeb72c21dd7366a36a98a760140ec2d51
51594 <51594@etu.he2b.be>
2020-06-10 17:13:07 +0200

    add critics table

--- database/migrations/2020_06_10_150646_create_critics_table.php
+++ database/migrations/2020_06_10_150646_create_critics_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFilmsTable extends Migration
+class CreateCriticsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,11 @@ class CreateFilmsTable extends Migration
      */
     public function up()
     {
-        Schema::create('films', function (Blueprint $table) {
+        Schema::create('critics', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('film_id');
+            $table->string('titre');
+            $table->text('contenu');
             $table->timestamps();
         });
     }
@@ -26,6 +29,6 @@ class CreateFilmsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('films');
+        Schema::dropIfExists('critics');
     }
 }
