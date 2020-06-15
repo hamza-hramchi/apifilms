b0722e9415cf97bda0a36dc567004bcb93b959d0
51594 <51594@etu.he2b.be>
2020-06-15 23:57:12 +0200

    Final Commit

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
+
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -26,7 +27,6 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
         Gate::define('isAdmin', function ($user) { return $user->type === 'admin'; });
         Gate::define('isUser', function ($user) { return $user->type === 'user'; });
-
 
     }
 }
--- resources/views/components/film-component.blade.php
+++ resources/views/components/film-component.blade.php
@@ -4,11 +4,5 @@
     </a>
     <div class="mt-2 ml-1">
         <a href="{{url('film/'.$film['id'])}}"  class="text-white text-lg mt-2">{{$film['title']}}</a>
-        <div class="felx items-center text-white text-sm">
-            <span class="text-black">
-                <i class="fa fa-calendar-alt"></i>
-                {{ \Carbon\Carbon::parse($film['release_date'])->format('d M Y')}}
-            </span>
-        </div>
     </div>
 </div>
